65f7e67f8e7af57e72b5b9add150c18f443e4fc9
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-07-07 17:21:25 -0500

    feat: :sparkles: base de datos

---
+++ db/db_M3_prueba_MYSQL2.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE db_M3_prueba_MYSQL2_node_joseDavid;
+
+
+USE db_M3_prueba_MYSQL2_node_joseDavid;
+
+CREATE TABLE tb_usuarios_M3(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nom_com VARCHAR(30) NOT NULL,
+    edad INTEGER NOT NULL
+);
